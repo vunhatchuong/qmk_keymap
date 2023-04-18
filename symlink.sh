e0b6bd9c0e119807809992cad5e6dfdc46cb51a0
Vu Nhat Chuong <ronnyvu321@gmail.com>
2023-04-18 13:32:29 +0700

    feat(script): Update symlink bash script

--- symlink.sh
+++ symlink.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-printf "----------"
+# dir of script 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+# parent dir of that dir
+QMK_FIRMWARE_DIR="${SCRIPT_DIR%/*}/qmk_firmware"
+
 printf "Making Symlink for userspace:"
-ln -s $HOME/qmk_keymap/users $HOME/qmk_firmware/users/ronny
+ln -s $SCRIPT_DIR/users $QMK_FIRMWARE_DIR/users/ronny
 
-printf "----------"
-printf "Making Symlink for Sofle:"
-ln -s $HOME/qmk_keymap/sofle $HOME/qmk_firmware/keyboards/sofle/keymaps/ronny
+printf "\nMaking Symlink for Sofle:"
+ln -s $SCRIPT_DIR/sofle $QMK_FIRMWARE_DIR/keyboards/sofle/keymaps/ronny
 
-printf "----------"
-printf "Making Symlink for Ferris Sweep:"
-ln -s $HOME/qmk_keymap/sweep $HOME/qmk_firmware/keyboards/ferris/keymaps/ronny
+printf "\nMaking Symlink for Ferris Sweep:"
+ln -s $SCRIPT_DIR/sweep $QMK_FIRMWARE_DIR/keyboards/ferris/keymaps/ronny
